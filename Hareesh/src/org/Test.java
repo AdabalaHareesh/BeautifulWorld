2022c4cd3e621d60ba073677712b95c048ba826c
AdabalaHareesh <Hareesh.a@datamedu.com>
2015-08-26 17:43:19 +0530

    Test java

--- Hareesh/src/org/Test.java
+++ Hareesh/src/org/Test.java
@@ -4,7 +4,11 @@ public class Test {
 
 public static void main(String arg[])
 	{
-		System.out.println("Hello");
-		System.out.println("Hi");
+	int a,b,c;
+	a= 5;
+	b= 10;
+	c= a + b;
+	System.out.println("sum of a and b= "+c);
+		
 	}
 }
